8c676a76d21d533291f8de94fb3c988ea3ff517e
Virginia Chen <virginia.chen32@gmail.com>
2019-02-12 23:47:08 -0800

    Improve speed of queue implemented using stacks

--- prework_2.py
+++ prework_2.py
@@ -33,7 +33,30 @@ print(reverse_list([1,2,3,4,5]))
 
 
 # Implement a queue using stacks
-class StackQueue(object):
+class FastStackQueue(object):
+    def __init__(self):
+        self._stack_1 = PythonStack()
+        self._stack_2 = PythonStack()
+
+    def is_empty(self):
+        return self._stack_1.is_empty() and self._stack_2.is_empty()
+
+    def enqueue(self, item):
+        # adds to the beginning of the queue
+        self._stack_1.push(item)
+
+    def dequeue(self):
+        # remove from end of queue
+        if self._stack_2.is_empty():
+            while self._stack_1.size() > 0:
+                self._stack_2.push(self._stack_1.pop())
+        return self._stack_2.pop()
+
+    def size(self):
+        return self._stack_1.size() + self._stack_2.size()
+
+
+class SlowStackQueue(object):
     def __init__(self):
         self._items = PythonStack()
         self._temp = PythonStack()
@@ -56,11 +79,14 @@ class StackQueue(object):
     def size(self):
         return self._items.size()
 
+
 # test
-queue = StackQueue()
-queue.enqueue("a")
-queue.enqueue("b")
-queue.enqueue("c")
-queue.dequeue()
-while queue.size() > 0:
-    print(queue.dequeue())
+n = 100000
+q = FastStackQueue()
+
+for i in range(n):
+    q.enqueue(i)
+
+for i in range(n):
+    print(q.dequeue())
+    q.enqueue(i)
